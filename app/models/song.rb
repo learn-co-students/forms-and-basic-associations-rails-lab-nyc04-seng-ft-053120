2f70459130528ac8f18850bcf788e90308c07ce0
BarbaraBontempo <barbarabontempo94@gmail.com>
2020-07-21 22:18:12 -0400

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,3 +1,31 @@
 class Song < ActiveRecord::Base
   # add associations here
-end
+  belongs_to :artist
+  belongs_to :genre
+  has_many :notes
+
+  def artist_name=(name)
+    self.artist = Artist.find_or_create_by(name: name)
+  end
+
+  def artist_name
+    self.artist ? self.artist.name : nil
+  end
+
+  def genre_name=(name)
+    self.genre = Genre.find_or_create_by(name)
+  end
+
+  def genre_name
+    self.genre ? self.genre.name : nil
+  end
+
+  def note_contents=(contents)
+    contents.each do |content|
+      a_content = Note.find_or_create_by(content: content)
+      self.notes << a_content 
+    end
+  end
+
+
+end #end of class
